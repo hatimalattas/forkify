3ab7243989d9ea8148d0e7c1acd176ba13a93f83
hatimalattas <alattas96@outlook.com>
2020-03-25 14:35:43 +0300

    implemented search result pagination

--- src/js/index.js
+++ src/js/index.js
@@ -37,3 +37,14 @@ elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    // console.log(btn);
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
+
